954767a0d958516519032af25b22222baf09e425
Samuel Gurský <samkogursky@gmail.com>
2019-06-04 12:48:53 +0200

    added filtered datasets with heuristic window

--- fastq_to_lines.py
+++ fastq_to_lines.py
@@ -1,14 +1,15 @@
 # quick script to re-write sequences from source file to more readily usable .txt
 
+filename = input()
 sequences = []
 
-with open('sequences/X8_15C.fastq','r') as data:
+with open('sequences/' + filename + '.fastq','r') as data:
     lines = data.readlines()
     assert len(lines) % 4 == 0
     for i in range(len(lines)//4):
         sequences.append( lines[i*4+1].strip() )
 
-with open('sequences/X8_15C.txt', 'w+') as output:
+with open('sequences/' + filename + '.txt', 'w+') as output:
     output.write(str(len(sequences)) + "\n")
     for seq in sequences:
         output.write(seq + "\n")    
